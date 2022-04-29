8da9332f971dd955cd741604360f9785d50ae2e0
Connor Szczepaniak <cszczepaniak@gmail.com>
2022-04-28 22:39:05 -0500

    run unit tests; consolidate publishing

--- scripts/pre-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-source ./scripts/variables.sh
-
-cd infrastructure/
-cdk deploy $INFRASTRUCTURE_STACK --require-approval=never
-cd -
--- scripts/publish-lambda.sh
+++ scripts/publish-lambda.sh
@@ -4,4 +4,8 @@ set -ex
 
 source ./scripts/variables.sh
 
+cd infrastructure/
+cdk deploy $INFRASTRUCTURE_STACK --require-approval=never
+cd -
+
 aws s3 cp $BUILD_HASH.zip s3://$CRIBBLY_APP_BUCKET/
