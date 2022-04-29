108ad85184e209907a0a572c4953c7607f0614e8
Connor Szczepaniak <cszczepaniak@gmail.com>
2022-04-28 22:18:37 -0500

    test on PR

--- scripts/build-lambda.sh
+++ scripts/build-lambda.sh
@@ -11,5 +11,3 @@ cd -
 GOOS=linux CGO_ENABLED=0 go build -o cribbly-backend cmd/lambda/main.go
 zip $BUILD_HASH.zip cribbly-backend
 rm cribbly-backend
-
-aws s3 cp $BUILD_HASH.zip s3://$CRIBBLY_APP_BUCKET/
