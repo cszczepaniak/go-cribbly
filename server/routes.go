5641a86396a6e437976098d0cadad7aa24c12390
Connor Szczepaniak <cszczepaniak@gmail.com>
2022-04-29 20:15:44 -0500

    remove test routes

--- server/routes.go
+++ server/routes.go
@@ -10,46 +10,4 @@ func (s *server) registerRoutes() {
 	s.eng.GET(`/ping`, func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, `pong`)
 	})
-
-	s.eng.POST(`/testdata/:key`, func(ctx *gin.Context) {
-		k := ctx.Params.ByName(`key`)
-
-		err := s.byteStore.Put(k, ctx.Request.Body)
-		defer ctx.Request.Body.Close()
-		if err != nil {
-			ctx.String(http.StatusInternalServerError, `%v`, err)
-			return
-		}
-
-		ctx.Status(http.StatusCreated)
-	})
-
-	s.eng.GET(`/testdata/:key`, func(ctx *gin.Context) {
-		p := ctx.Params.ByName(`key`)
-
-		resp, err := s.byteStore.Get(p)
-		if err != nil {
-			ctx.String(http.StatusInternalServerError, `%v`, err)
-			return
-		}
-
-		ctx.String(http.StatusOK, `%s`, resp)
-	})
-
-	s.eng.GET(`/testdata/many/:prefix`, func(ctx *gin.Context) {
-		p := ctx.Params.ByName(`prefix`)
-
-		resp, err := s.byteStore.GetWithPrefix(p)
-		if err != nil {
-			ctx.String(http.StatusInternalServerError, `%v`, err)
-			return
-		}
-
-		res := make(map[string]string, len(resp))
-		for k, v := range resp {
-			res[k] = string(v)
-		}
-
-		ctx.JSON(http.StatusOK, res)
-	})
 }
